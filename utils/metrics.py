2408df159f993318d9a1bffd0a3a799af21fae8c
Kirill <kirillribkin1998@gmail.com>
2020-11-21 16:22:32 +0300

    Исправленна метрика

--- utils/metrics.py
+++ utils/metrics.py
@@ -29,21 +29,23 @@ def smape_loss():
     Функция, для передачи метрики керасу
     :return: функция потерь
     """
+
     def loss(satellite_predicted_values, satellite_true_values):
         return K.mean(K.abs((satellite_predicted_values - satellite_true_values)
                             / (K.abs(satellite_predicted_values) + K.abs(satellite_true_values))))
 
     return loss
 
-def roc_auc_score_at_K(predicted_proba, target, rate=0.1): 
-	"""
-	Area under the ROC curve between the predicted probability and the observed target.
-	The area under the ROC curve will be calculated only on the top 10 percent of predictions sorted descendingly.
-	:param predicted_proba: предсказанное значение
+
+def roc_auc_score_at_K(predicted_proba, target, rate=0.1):
+    """
+    Area under the ROC curve between the predicted probability and the observed target.
+    The area under the ROC curve will be calculated only on the top 10 percent of predictions sorted descendingly.
+    :param predicted_proba: предсказанное значение
     :param target: истинное значение
     :param rate: процент меток
-	:return: roc auc score at k
-	"""
-	order = np.argsort(-predicted_proba) 
-	top_k = int(rate ∗ len(predicted_proba)) 
-	return roc_auc_score(target[order][:top_k], predicted_proba[order][:top_k])
+    :return: roc auc score at k
+    """
+    order = np.argsort(-predicted_proba)
+    top_k = int(rate * len(predicted_proba))
+    return roc_auc_score(target[order][:top_k], predicted_proba[order][:top_k])
