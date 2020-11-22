8368ff0eae4e7d9296b695996525ea01e5ccfea6
Kirill <kirillribkin1998@gmail.com>
2020-11-22 14:58:55 +0300

    Посылка CatBoost

--- models/catboost_regression/catboost_regression.py
+++ models/catboost_regression/catboost_regression.py
@@ -16,12 +16,12 @@ class CatboostRegressor(Model):
     default_model_constructor_parameters = {
         'iterations': 1000,
         'learning_rate': 3e-2,
-        'l2_leaf_reg': 3.0,# any pos value
-        'depth': 6, # int up to 16
-        'min_data_in_leaf': 1, #1,2,3,4,5
-        'rsm': 1, #0.01 .. 1.0
+        'l2_leaf_reg': 3.0,  # any pos value
+        'depth': 6,  # int up to 16
+        'min_data_in_leaf': 1,  # 1,2,3,4,5
+        'rsm': 1,  # 0.01 .. 1.0
         'langevin': False,
-        'diffusion_temperature': 10000 # 100 ... 100000 
+        'diffusion_temperature': 10000  # 100 ... 100000
     }
 
     def __init__(self):
@@ -33,15 +33,14 @@ class CatboostRegressor(Model):
         :param parameters: Гиперпараметры модели
         """
         self.model = CatBoostRegressor(iterations=parameters['iterations'],
-                                        learning_rate=parameters['learning_rate'],
-                                        l2_leaf_reg=parameters['l2_leaf_reg'],
-                                        depth=parameters['depth'],
-                                        min_data_in_leaf=parameters['min_data_in_leaf'],
-                                        rsm=parameters['rsm'],
-                                        langevin=parameters['langevin'],
-                                        diffusion_temperature=parameters['diffusion_temperature'],
-                                        random_state=c.SEED,
-                                        verbose=0)
+                                       learning_rate=parameters['learning_rate'],
+                                       l2_leaf_reg=parameters['l2_leaf_reg'],
+                                       depth=parameters['depth'],
+                                       min_data_in_leaf=parameters['min_data_in_leaf'],
+                                       loss_function="RMSE", eval_metric='AUC', rsm = 1,
+                                       diffusion_temperature=parameters['diffusion_temperature'],
+                                       random_state=c.SEED,
+                                       verbose=100)
 
     def fit_model(self, x, y, test_size=0.2) -> float:
         """
@@ -50,13 +49,8 @@ class CatboostRegressor(Model):
         """
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=c.SEED)
         self.model.fit(x_train, y_train)
-        y_prediction = self.model.predict(x_test)
-        return self.score(y_test, y_prediction)
+        return self.score(x_test, y_test)
 
     def score(self, x, y):
-        """
-        Определение точности модели. Шаблон
-        :return: rmse
-        """
         y_prediction = self.predict(x)
         return -roc_auc_score_at_K(reset_averages(y_prediction), y, rate=0.1)
